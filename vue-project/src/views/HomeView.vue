1ed43278301f4690fd8e24cc1fc34876bd1445ba
ashikgligich <ashton.gligich@gmail.com>
2024-03-05 09:18:24 -0500

    ez
    e

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import chartChaer from '../components/chartChaer.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+   
+    <chartChaer />
   </main>
 </template>
