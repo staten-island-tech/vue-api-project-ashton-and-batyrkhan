c8c66bfde4ccb9e2644c6cd62e56618210540c53
Batyrkhan Alimzhanov <batyrkhansa@gmail.com>
2024-03-07 09:24:33 -0500

    d1

--- vue-project/src/components/chartChaer.vue
+++ vue-project/src/components/chartChaer.vue
@@ -1,30 +1,32 @@
 <template>
-    <Bar
-      id="my-chart-id"
-      :options="chartOptions"
-      :data="chartData"
-    />
-  </template>
+  <Bar id="my-chart-id" :options="chartOptions" :data="chartData" />
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
+</template>
   
-  <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-  
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-  
-  export default {
-    name: 'BarChart',
-    components: { Bar },
-    data() {
-      return {
-        chartData: {
-          labels: [ 'January', 'February', 'March' ],
-          datasets: [ { data: [40, 3800, 12] } ]
-        },
-        chartOptions: {
-          responsive: true
-        }
-      }
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted() {
+    try {
+      const { NYCData } = await fetch('https://data.cityofnewyork.us/resource/gzfs-3h4m.json');
+      this.chartData = NYCData
+      this.loaded = true
+      console.log(NYCData)
+    } catch (e) {
+      console.error(e)
     }
   }
-  </script>
+}
+</script>
