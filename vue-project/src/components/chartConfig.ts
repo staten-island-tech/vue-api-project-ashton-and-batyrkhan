d91c04fd6d6b20403eb464e6a86226ebb7e1e8b9
ashikgligich <ashton.gligich@gmail.com>
2024-03-20 09:24:14 -0400

    A
    ,MMMM

--- vue-project/src/components/chartConfig.ts
+++ vue-project/src/components/chartConfig.ts
@@ -4,20 +4,13 @@ export const data = {
       'February',
       'March',
       'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December'
+      
     ],
     datasets: [
       {
         label: 'Data One',
         backgroundColor: '#f87979',
-        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+        data: [40, 20, 12, 39]
       }
     ]
   }
