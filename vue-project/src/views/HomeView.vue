4ff4c8e730ad3b3a31553671b316a70ec159e5dc
ashikgligich <144455072+ashikgligich@users.noreply.github.com>
2024-03-19 20:24:13 -0400

    edrfkjuhk
    ee

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
 import chartChaer from '../components/chartChaer.vue'
+import CharOno from '../components/CharOno.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
     <chartChaer />
+    <CharOno />
   </main>
 </template>
 
