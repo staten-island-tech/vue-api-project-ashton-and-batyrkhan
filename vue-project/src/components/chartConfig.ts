ef2baf71dafbec92690ad1fd70eb20792f65064f
ashikgligich <ashton.gligich@gmail.com>
2024-03-21 09:22:02 -0400

    yipeeeeeeeeeeeeeeeeeeeeeeeeeeee
    chart NOW WITH RADAR

--- vue-project/src/components/chartConfig.ts
+++ vue-project/src/components/chartConfig.ts
@@ -1,16 +1,44 @@
 export const data = {
     labels: [
-      'January',
-      'February',
-      'March',
-      'April',
+      'nyc transit',
+      'nyc public restrooms',
+      'nyc garbage collection',
+      'nyc public schools',
       
     ],
     datasets: [
       {
-        label: 'Data One',
-        backgroundColor: '#f87979',
+        label: 'funding',
+        //backgroundColor: 'rgba(179,181,198,0.2)',
+        borderColor: 'rgba(179,181,198,1)',
+        pointBackgroundColor: 'rgba(179,181,198,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(179,181,198,1)',
+        backgroundColor: 'rgba(100, 100, 255,0.2)',
         data: [40, 20, 12, 39]
+      },
+      {
+        label: 'spending',
+        //backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(100,155,100,1)',
+        pointBackgroundColor: 'rgba(100,155,100,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(100,155,100,1)',
+        backgroundColor: 'rgba(100, 255, 100,0.2)',
+        data: [50, 30, 22, 49]
+      },
+      {
+        label: 'headcount',
+        //backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        pointBackgroundColor: 'rgba(255,99,132,1)',
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(255,99,132,1)',
+        backgroundColor: 'rgba(255, 100, 100,0.2)',
+        data: [20, 10, 2, 29]
       }
     ]
   }
--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -2,13 +2,19 @@
 import TheWelcome from '../components/TheWelcome.vue'
 import chartChaer from '../components/chartChaer.vue'
 import CharOno from '../components/CharOno.vue'
+import radarDos from '../components/radarDos.vue'
+
+
+
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    
     <chartChaer />
     <CharOno />
+    <radarDos />
   </main>
 </template>
 
